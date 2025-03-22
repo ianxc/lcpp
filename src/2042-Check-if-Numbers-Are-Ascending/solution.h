f9529781bb681e98a4d8f2169d12322bd4c8759c
Ian Chen <ianchen7777@gmail.com>
2025-03-22 20:50:16 +1100

    2042 Check If Numbers Are Ascending

--- src/2042-Check-if-Numbers-Are-Ascending/solution.h
+++ src/2042-Check-if-Numbers-Are-Ascending/solution.h
@@ -1,9 +1,45 @@
 #pragma once
 
+#include <cctype>
+#include <icecream.hpp>
+#include <string>
+
 class Solution {
   public:
-    int find_solution(int x) {
-        auto y = 3 * x;
-        return y - 2;
+    bool areNumbersAscending(std::string s) {
+        IC(s);
+
+        int length = s.size();
+        auto last_num = -1, curr_num = 0, i = 0;
+        auto curr_is_digit = false;
+        while (i < length) {
+            IC(i, s[i]);
+            if (s[i] == ' ') {
+                IC(i, s[i], curr_num, last_num, curr_is_digit, "a");
+                // reset everything
+                if (curr_is_digit) {
+                    if (last_num >= curr_num) return false;
+                    last_num = curr_num;
+                    curr_num = 0;
+                    curr_is_digit = false;
+                }
+
+                i++;
+                continue;
+            }
+            IC(i, s[i], curr_is_digit);
+            if (!curr_is_digit && std::isdigit(s[i])) {
+                IC(i, s[i], "d");
+                curr_is_digit = true;
+            }
+
+            if (curr_is_digit) {
+                curr_num = curr_num * 10 + (s[i] - '0');
+            }
+            IC(i, s[i], curr_num, last_num);
+            i++;
+        };
+
+        return true;
     }
 };
--- test/2042-Check-if-Numbers-Are-Ascending/solution.test.cpp
+++ test/2042-Check-if-Numbers-Are-Ascending/solution.test.cpp
@@ -5,5 +5,7 @@
 TEST(test_suite_name, test_name) {
     Solution s;
 
-    EXPECT_EQ(s.find_solution(12), 34);
+    EXPECT_EQ(s.areNumbersAscending("a puppy has 2 eyes 4 legs"), true);
+    EXPECT_EQ(s.areNumbersAscending("a puppy has 4 eyes 2 legs"), false);
+    EXPECT_EQ(s.areNumbersAscending("12 a 23 45 100 b cz"), true);
 }
