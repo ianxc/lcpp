b9681de77c0f1b2590a5fda2c17371bda4c1c62b
Ian Chen <ianchen7777@gmail.com>
2025-03-22 20:21:13 +1100

    Apples heap solution

--- src/1196-How-Many-Apples/solution.h
+++ src/1196-How-Many-Apples/solution.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <algorithm>
+#include <icecream.hpp>
 #include <vector>
 
 class Solution {
@@ -14,6 +15,26 @@ class Solution {
             if (sum > 5000) {
                 return i;
             }
+        }
+        return n;
+    }
+};
+
+class HeapSolution {
+  public:
+    int maxNumberOfApples(std::vector<int>& weight) {
+        // stl defaults to max-heap (python + java default to min-heap).
+        // want a min-heap here, so create a comparator and use it consistently.
+        auto cmp = [](int a, int b) -> bool { return a > b; };
+        std::make_heap(weight.begin(), weight.end(), cmp);
+
+        int n = weight.size();
+        int sum = 0;
+        for (auto i = 0; i < n; i++) {
+            sum += weight.front();
+            if (sum > 5000) return i;
+            std::pop_heap(weight.begin(), weight.end(), cmp);
+            weight.pop_back();
         }
         return n;
     }
--- test/1196-How-Many-Apples/solution.test.cpp
+++ test/1196-How-Many-Apples/solution.test.cpp
@@ -2,8 +2,18 @@
 
 #include <gtest/gtest.h>
 
-TEST(test_suite_name, test_name) {
+TEST(Solution1, basic) {
     Solution s;
+
+    std::vector in1{100, 200, 150, 1000};
+    EXPECT_EQ(s.maxNumberOfApples(in1), 4);
+
+    std::vector<int> in2{5000, 10};
+    EXPECT_EQ(s.maxNumberOfApples(in2), 1);
+}
+
+TEST(HeapSolution, basic) {
+    HeapSolution s;
 
     std::vector in1{100, 200, 150, 1000};
     EXPECT_EQ(s.maxNumberOfApples(in1), 4);
