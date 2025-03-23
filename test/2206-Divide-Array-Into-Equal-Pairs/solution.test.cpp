0fe8c80a16c99ab520c5d2211501cb720c054e52
Ian Chen <ianchen7777@gmail.com>
2025-03-23 17:43:46 +1100

    odd number of pairs whose pair sums = 2^n-1

--- test/2206-Divide-Array-Into-Equal-Pairs/solution.test.cpp
+++ test/2206-Divide-Array-Into-Equal-Pairs/solution.test.cpp
@@ -14,13 +14,14 @@ scenario t2{{1, 2, 3, 4}, false};
 // note how we can make pairs that sum to 7 = 2^3 - 1.
 scenario t3{{1, 2, 5, 6}, false};
 scenario t4{{1, 2, 7, 8}, false};
+scenario t5{{1, 2, 5, 6, 3, 4}, false};
 
 }  // namespace
 
 TEST(Solution, all) {
     Solution s;
 
-    for (auto sc : {&t1, &t2, &t3, &t4}) {
+    for (auto sc : {&t1, &t2, &t3, &t4, &t5}) {
         EXPECT_EQ(s.divideArray(sc->input), sc->expected);
     }
 }
@@ -28,7 +29,7 @@ TEST(Solution, all) {
 TEST(WrongXorSolution, all) {
     WrongXorSolution s;
 
-    for (auto sc : {&t1, &t2, &t4}) {
+    for (auto sc : {&t1, &t2, &t4, &t5}) {
         EXPECT_EQ(s.divideArray(sc->input), sc->expected);
     }
 
