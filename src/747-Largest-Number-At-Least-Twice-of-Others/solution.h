81a9d7922a7d92c3fdb29213f12058baf32cca3e
Ian Chen <ianchen7777@gmail.com>
2025-03-27 19:36:36 +1100

    747 Largest number at least twice of others

---
+++ src/747-Largest-Number-At-Least-Twice-of-Others/solution.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <vector>
+
+class Solution {
+  public:
+    int dominantIndex(std::vector<int>& nums) {
+        auto [best1, best2] =
+            nums[0] > nums[1] ? std::pair{0, 1} : std::pair{1, 0};
+
+        auto n = static_cast<int>(nums.size());
+        for (int i = 2; i < n; i++) {
+            if (nums[i] >= nums[best1]) {
+                best2 = best1;
+                best1 = i;
+            } else if (nums[i] > nums[best2]) {
+                best2 = i;
+            }
+        }
+
+        return nums[best1] >= 2 * nums[best2] ? best1 : -1;
+    }
+};
---
+++ test/747-Largest-Number-At-Least-Twice-of-Others/solution.test.cpp
@@ -0,0 +1,17 @@
+#include "solution.h"
+
+#include <gtest/gtest.h>
+
+#include <vector>
+
+TEST(test_suite_name, test_name) {
+    Solution s;
+
+    using scenario = std::pair<std::vector<int>, int>;
+
+    scenario s1{{3, 6, 1, 0}, 1};
+    scenario s2{{1, 2, 3, 4}, -1};
+
+    EXPECT_EQ(s.dominantIndex(s1.first), s1.second);
+    EXPECT_EQ(s.dominantIndex(s2.first), s2.second);
+}
