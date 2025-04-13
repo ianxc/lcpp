51f2b80cd1c8d93acf2da7d59b39164ffa19fda0
Ian Chen <ianchen7777@gmail.com>
2025-04-13 15:47:23 +1000

    75 Sort Colors - Dutch National Flag problem

---
+++ src/75-Sort-Colors/solution.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <icecream.hpp>
+#include <utility>
+#include <vector>
+
+namespace p75 {
+
+class Solution {
+  public:
+    void sortColors(std::vector<int>& nums) {
+        IC_CONFIG.output(std::cout);
+
+        const auto n = static_cast<int>(nums.size());
+        int left = 0, curr = 0, right = n - 1;
+
+        while (curr <= right) {
+            IC(left, curr, right, nums);
+            if (nums[curr] == 0) {
+                std::swap(nums[left], nums[curr]);
+                left++;
+                curr++;
+            } else if (nums[curr] == 2) {
+                std::swap(nums[curr], nums[right]);
+                right--;
+            } else {
+                curr++;
+            }
+        }
+    }
+};
+
+}  // namespace p75
