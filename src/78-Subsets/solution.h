7928ce8374e3ed2c22321e6cee981e472d636e99
Ian Chen <ianchen7777@gmail.com>
2025-04-20 18:55:57 +1000

    Subsets y-combinator

--- src/78-Subsets/solution.h
+++ src/78-Subsets/solution.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <functional>
 #include <vector>
 
 namespace p78 {
@@ -9,26 +8,25 @@ class Solution {
   public:
     std::vector<std::vector<int>> subsets(std::vector<int>& nums) {
         const int n = nums.size();
-        std::vector<std::vector<int>> res;
+        std::vector<std::vector<int>> saved_subsets;
 
-        // cannot use auto inline lambda prior to C++23 as it's recursive.
-        std::function<void(int, std::vector<int>&)> backtrack =
-            [&, n](int curr_idx, std::vector<int>& curr_nums) -> void {
-            res.push_back(curr_nums);
+        auto backtrack = [&, n](const auto& self, int curr_idx,
+                                std::vector<int>& curr_nums) -> void {
+            saved_subsets.push_back(curr_nums);
             if (curr_idx == n) {
                 return;
             }
 
             for (auto i = curr_idx; i < n; i++) {
                 curr_nums.push_back(nums[i]);
-                backtrack(i + 1, curr_nums);
+                self(self, i + 1, curr_nums);
                 curr_nums.pop_back();
             }
         };
 
         std::vector<int> v;
-        backtrack(0, v);
-        return res;
+        backtrack(backtrack, 0, v);
+        return saved_subsets;
     }
 };
 
