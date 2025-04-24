8e3fabbfabbeca8ccd4934d4004c36560ca542a8
Ian Chen <ianchen7777@gmail.com>
2025-04-24 23:10:23 +1000

    multi-digit

--- src/604-Design-Compressed-String-Iterator/solution.h
+++ src/604-Design-Compressed-String-Iterator/solution.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cassert>
+#include <cctype>
 #include <icecream.hpp>
 #include <string>
 
@@ -10,47 +11,55 @@ int ctoi(char c) { return c - '0'; }
 
 class StringIterator {
   private:
-    size_t curr_idx;
-    int curr_rem;
     std::string source_string;
+    char curr_char;
+    int curr_rem;
+    size_t next_char_idx;
 
   public:
     StringIterator(std::string compressedString) {
-        this->source_string = std::move(compressedString);
-        this->curr_idx = 0;
-        if (this->source_string.length() >= 2) {
-            auto first_rem = ctoi(this->source_string[1]);
-            assert(first_rem > 0);
-            this->curr_rem = first_rem;
-        } else {
-            this->curr_rem = 0;
+        this->curr_char = compressedString[0];
+
+        this->curr_rem = 0;
+        auto curr_rem_idx = 1u;
+        while (curr_rem_idx < compressedString.length() &&
+               std::isdigit(compressedString[curr_rem_idx])) {
+            this->curr_rem *= 10;
+            this->curr_rem += ctoi(compressedString[curr_rem_idx]);
+            curr_rem_idx++;
         }
+        this->next_char_idx = curr_rem_idx;
+
+        this->source_string = std::move(compressedString);
     }
 
     char next() {
         if (!this->hasNext()) {
             return ' ';
         }
-
-        assert(this->curr_rem > 0);
-
         if (curr_rem == 1) {
             // prepare internal state to point to the next character
-            auto to_return = this->source_string[this->curr_idx];
-            this->curr_idx += 2;
-            auto next_rem_idx = this->curr_idx + 1;
-            this->curr_rem = next_rem_idx < this->source_string.length()
-                                 ? ctoi(this->source_string[next_rem_idx])
-                                 : 0;
+            auto to_return = curr_char;
+            this->curr_char = this->source_string[this->next_char_idx];
+            this->curr_rem = 0;
+            auto curr_rem_idx = this->next_char_idx + 1;
+            while (curr_rem_idx < this->source_string.length() &&
+                   std::isdigit(this->source_string[curr_rem_idx])) {
+                this->curr_rem *= 10;
+                this->curr_rem += ctoi(this->source_string[curr_rem_idx]);
+                curr_rem_idx++;
+            }
+            this->next_char_idx = curr_rem_idx;
+
             return to_return;
         }
 
         assert(this->curr_rem > 1);
         this->curr_rem--;
-        return this->source_string[this->curr_idx];
+        return this->curr_char;
     }
 
-    bool hasNext() { return this->curr_idx < this->source_string.length(); }
+    bool hasNext() { return this->curr_rem > 0; }
 };
 
 }  // namespace p604
--- src/604-Design-Compressed-String-Iterator/solution.test.cpp
+++ src/604-Design-Compressed-String-Iterator/solution.test.cpp
@@ -16,3 +16,18 @@ TEST(iterator, basic) {
     EXPECT_FALSE(s.hasNext());
     EXPECT_EQ(s.next(), ' ');
 }
+
+TEST(iterator, multi_digit_freq) {
+    StringIterator s("a21b2c3");
+
+    for (int i = 0; i < 21; i++) {
+        EXPECT_EQ(s.next(), 'a');
+    }
+    EXPECT_EQ(s.next(), 'b');
+    EXPECT_EQ(s.next(), 'b');
+    EXPECT_EQ(s.next(), 'c');
+    EXPECT_EQ(s.next(), 'c');
+    EXPECT_EQ(s.next(), 'c');
+    EXPECT_FALSE(s.hasNext());
+    EXPECT_EQ(s.next(), ' ');
+}
