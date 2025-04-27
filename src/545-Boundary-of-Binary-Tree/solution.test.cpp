8ef0316e9e5bcfb8d1f143fcb3c2bce46d5690b1
Ian Chen <ianchen7777@gmail.com>
2025-04-27 13:02:00 +1000

    no explicit delete in test

--- src/545-Boundary-of-Binary-Tree/solution.test.cpp
+++ src/545-Boundary-of-Binary-Tree/solution.test.cpp
@@ -7,27 +7,24 @@ using namespace p545;
 TEST(areNumbersAscending, LC_1) {
     Solution s;
 
-    auto *input = new TreeNode(
-        1, nullptr, new TreeNode(2, new TreeNode(3), new TreeNode(4)));
+    TreeNode input{1, nullptr,
+                   new TreeNode{2, new TreeNode{3}, new TreeNode{4}}};
     std::vector exp = {1, 3, 4, 2};
 
-    EXPECT_EQ(s.boundaryOfBinaryTree(input), exp);
-
-    delete input;
+    EXPECT_EQ(s.boundaryOfBinaryTree(&input), exp);
 }
 
 TEST(areNumbersAscending, LC_2) {
     Solution s;
 
-    auto *input = new TreeNode(
+    auto input = TreeNode{
         1,
         new TreeNode(2, new TreeNode(4),
                      new TreeNode(5, new TreeNode(7), new TreeNode(8))),
         new TreeNode(3, new TreeNode(6, new TreeNode(9), new TreeNode(10)),
-                     nullptr));
+                     nullptr)};
+
     std::vector exp = {1, 2, 4, 7, 8, 9, 10, 6, 3};
 
-    EXPECT_EQ(s.boundaryOfBinaryTree(input), exp);
-
-    delete input;
+    EXPECT_EQ(s.boundaryOfBinaryTree(&input), exp);
 }
