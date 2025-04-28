8266f94551db75a80c71e2ba5af401090a85baa1
Ian Chen <ianchen7777@gmail.com>
2025-04-29 01:23:50 +1000

    single pop_back

--- src/370-Range-Addition/solution.h
+++ src/370-Range-Addition/solution.h
@@ -9,19 +9,18 @@ class Solution {
   public:
     std::vector<int> getModifiedArray(int length,
                                       std::vector<std::vector<int>>& updates) {
-        std::vector<int> deltas(length);
+        const int n = length + 1;
+        std::vector<int> deltas(n);
         for (auto u : updates) {
             auto start = u[0], end_excl = u[1] + 1, incr = u[2];
             deltas[start] += incr;
-            if (end_excl < length) {
-                deltas[end_excl] -= incr;
-            }
+            deltas[end_excl] -= incr;
         }
-
-        for (int i = 1; i < length; i++) {
+        for (int i = 1; i < n; i++) {
             deltas[i] += deltas[i - 1];
         }
 
+        deltas.pop_back();
         return deltas;
     };
 };
