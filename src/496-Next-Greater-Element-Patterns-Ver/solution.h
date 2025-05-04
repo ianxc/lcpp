ddf4b4d584e1809e599c5335afcd589b09fafcb8
Ian Chen <ianchen7777@gmail.com>
2025-05-04 20:23:15 +1000

    Also detail the forward approach

--- src/496-Next-Greater-Element-Patterns-Ver/solution.h
+++ src/496-Next-Greater-Element-Patterns-Ver/solution.h
@@ -5,6 +5,10 @@
 
 namespace p496_patterns_ver {
 
+/**
+ * Approach: monotonically decreasing stack (smallest values at topic of stack)
+ * stores the valid remanining candidates for the next greater value.
+ */
 class IterateReverseSolution {
   public:
     std::vector<int> find_solution(const std::vector<int>& nums) {
@@ -22,6 +26,30 @@ class IterateReverseSolution {
             }
 
             next_greater_candidates.push(nums[i]);
+        }
+
+        return next_greater_results;
+    }
+};
+
+/**
+ * Approach: monotonically increasing stack that stores previous values for
+ * which we have not yet found a greater element for (actually, store the
+ * index).
+ */
+class IterateForwardSolution {
+  public:
+    std::vector<int> find_solution(const std::vector<int>& nums) {
+        const int n = nums.size();
+        std::vector<int> next_greater_results(n, -1);
+        std::stack<int> greater_to_find;
+        for (int i = 0; i < n; i++) {
+            while (!greater_to_find.empty() &&
+                   nums[greater_to_find.top()] < nums[i]) {
+                next_greater_results[greater_to_find.top()] = nums[i];
+                greater_to_find.pop();
+            }
+            greater_to_find.push(i);
         }
 
         return next_greater_results;
--- src/496-Next-Greater-Element-Patterns-Ver/solution.test.cpp
+++ src/496-Next-Greater-Element-Patterns-Ver/solution.test.cpp
@@ -13,12 +13,21 @@ struct TestCase {
 
 std::vector<TestCase> cases{
     {.nums = {1, 1, 2, 3, 2, 3, 2, 4}, .expected = {2, 2, 3, 4, 3, 4, 4, -1}},
+    {.nums = {1, 1, 2, 3, 2, 5, 2, 4}, .expected = {2, 2, 3, 5, 5, -1, 4, -1}},
 };
 
 }  // namespace
 
 TEST(IterateReverseSolutionTest, test_name) {
     IterateReverseSolution s;
+
+    for (auto& [nums, expected] : cases) {
+        EXPECT_EQ(s.find_solution(nums), expected);
+    }
+};
+
+TEST(IterateForwardSolutionTest, test_name) {
+    IterateForwardSolution s;
 
     for (auto& [nums, expected] : cases) {
         EXPECT_EQ(s.find_solution(nums), expected);
